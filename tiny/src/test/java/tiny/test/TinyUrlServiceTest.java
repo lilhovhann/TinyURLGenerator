8db7080b3f201876d6dc4512827303ea8e5cfde2
lilhovhann <lilith.hovhann@gmail.com>
2021-12-24 14:57:10 +0400

    d

--- tiny/src/test/java/tiny/test/TinyUrlServiceTest.java
+++ tiny/src/test/java/tiny/test/TinyUrlServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tiny.services.TinyUrlService;
 
@@ -15,16 +16,13 @@ import tiny.services.TinyUrlService;
  */
 @RunWith(SpringRunner.class)
 @AutoConfigureWebMvc
-//@SpringBootTest
 public class TinyUrlServiceTest {
 
- 
     private TinyUrlService tinyUrlService = new TinyUrlService();
 
     @Test
     public void letsTest() {
         String result = tinyUrlService.letsTest("Lilit");
-//        String result = "Hey";
         System.out.println("Hey");
         Assert.assertEquals("Hi Lilit", result);
     }
